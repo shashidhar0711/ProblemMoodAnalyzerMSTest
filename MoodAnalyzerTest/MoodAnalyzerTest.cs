bf0aa6fa9c2b4777fee1d7e9695b44781eb2f967
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-30 09:31:15 +0530

    [sasic] Refactor . Using Customized Exception to Handled Exception

--- MoodAnalyzerTest/MoodAnalyzerTest.cs
+++ MoodAnalyzerTest/MoodAnalyzerTest.cs
@@ -4,20 +4,26 @@ using ProblemMoodAnalyzerMSTest;
 namespace MoodAnalyzerTest
 {
     [TestClass]
-    public class UnitTest1
+    public class MoodAnalyzerTest
     {
+        /// <summary>
+        /// Given the empty mood should throw mood cannot be empty.
+        /// </summary>
         [TestMethod]       
         public void GivenEmptyMoodShouldThrowMoodCannotBeEmpty()
         {
             try
             {
+                /// Arrange
                 string message = "";
                 MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
 
+                /// Act
                 string mood = moodAnalyzer.AnalyzeMood();
             }
             catch(AnalyzeMoodCustomizedException Exception)
             {
+                /// Assert
                 Assert.AreEqual("Mood should not be empty", Exception.Message);
             }
         }
@@ -25,11 +31,28 @@ namespace MoodAnalyzerTest
         [TestMethod]
         public void GivenEmptyMoodShouldThrowMoodCannotBeNull()
         {
+            /// Arrange
             string message = "I am not in mood";
             MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
 
+            /// Act
             string mood = moodAnalyzer.AnalyzeMood();
 
+            /// Assert
+            Assert.AreEqual("happy", mood);
+        }
+
+        [TestMethod]
+        public void GivenEmptyMoodShouldThrowMoodCannotBeNullAgain()
+        {
+            /// Arrange
+            string message = "NULL";
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
+
+            /// Act
+            string mood = moodAnalyzer.AnalyzeMood();
+
+            /// Assert
             Assert.AreEqual("happy", mood);
         }
     }
